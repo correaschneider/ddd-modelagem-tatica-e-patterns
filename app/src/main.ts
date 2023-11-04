27e520e25870df9fd6f160c208e88671db21cacb
Pedro Schneider <correaschneider@gmail.com>
2023-11-04 14:23:26 -0300

    Adicionando bloqueio de validação do tsc

--- app/src/main.ts
+++ app/src/main.ts
@@ -8,6 +8,6 @@ const address = new Address('Servidão Pedro Manoel Fernandez', 63, '88062400', 
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem('1', 'Item 1', 10);
-const item2 = new OrderItem('2', 'Item 2', 15);
+const item1 = new OrderItem('i1', 'p1', 'Item 1', 10, 2);
+const item2 = new OrderItem('i2', 'p2', 'Item 2', 15);
 const order = new Order('1', '123', [item1, item2]);
